1393362a0fa2faf218eb46c088d11ce07569432a
DanSeb1295 <daniel-sebastian95@hotmail.com>
2019-03-08 22:01:28 -0800

    update

--- app.py
+++ app.py
@@ -160,6 +160,6 @@ def logout():
 	return redirect(url_for('home'))
 
 # if __name__ == '__main__':
-	# app.run(debug=True, ssl_context=('./ssl.crt', './ssl.key'))
+# 	app.run(debug=True, ssl_context=('./ssl.crt', './ssl.key'))
 # 	port = int(os.environ.get('PORT', 5000))
 # 	app.run(host='0.0.0.0', port=port, ssl_context=('./ssl.crt', './ssl.key'))
--- config/config.py
+++ config/config.py
@@ -11,7 +11,7 @@ class Auth:
     AUTH_URI = 'https://accounts.google.com/o/oauth2/auth'
     TOKEN_URI = 'https://accounts.google.com/o/oauth2/token'
     USER_INFO = 'https://www.googleapis.com/userinfo/v2/me'
-    SCOPE = ['profile', 'email']
+    SCOPE = ['https://www.googleapis.com/auth/userinfo.email', 'https://www.googleapis.com/auth/userinfo.profile']
 
 class Config:
     """Base config"""
